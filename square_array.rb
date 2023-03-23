0bfc34bf4ba88ce745aeee75901533fef401eaa1
Lluvia <friasllu@gmail.com>
2023-03-22 20:44:44 -0400

    commit tree cfa3f811159a9ea53420d8d46dde7f7476b36189

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  squared_arr =[]
+  array.each { |i| squared_arr << i ** 2 } 
+  squared_arr
 end
+
+#def square_array(array)
+#    array.collect { |i| i ** 2 }
+#end 
